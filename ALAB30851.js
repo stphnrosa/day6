44c20ab5af42004bff8c23fb064af7f070bd0c4f
stphnrosa <stphn.rosa@gmail.com>
2025-03-06 22:31:51 -0500

    fourth commit

--- ALAB30851.js
+++ ALAB30851.js
@@ -1,2 +1,41 @@
 // //Create reusable blocks of code with functions.
 // Implement Array object methods.
+
+
+
+
+
+
+// write functions that accomplish the following
+//Take an array of numbers and return the sum.
+
+
+
+//  Take an array of numbers and return the average.
+//Take an array of strings and return the longest string.
+//Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+
+
+
+
+
+
+// //For example,
+// //  stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+// // Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+
+ n= 12
+function count(n){
+
+if (n <=12) { //This is where we stop counting
+    console.log("final"); // message that will be shown once we reach 0
+    return;
+
+}
+
+console.log(n);
+count(1);
+}
+
+count(1);
+
--- sba308.js
+++ sba308.js
@@ -67,34 +67,34 @@
 
 // SAMPLEDATA(sandbox):
 const CourseInfo = {
-    id: 451,
+    id: ,
     name: "Introduction to JavaScript"
   };
   
   // The provided assignment group.
   const AssignmentGroup = {
-    id: 12345,
+    id: ,
     name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
+    course_id: ,
+    group_weight: ,
     assignments: [
       {
-        id: 1,
+        id: ,
         name: "Declare a Variable",
         due_at: "2023-01-25",
-        points_possible: 50
+        points_possible: 
       },
       {
-        id: 2,
+        id: ,
         name: "Write a Function",
         due_at: "2023-02-27",
-        points_possible: 150
+        points_possible: 
       },
       {
         id: 3,
         name: "Code the World",
         due_at: "3156-11-15",
-        points_possible: 500
+        points_possible: 
       }
     ]
   };
@@ -102,43 +102,43 @@ const CourseInfo = {
   // The provided learner submission data.
   const LearnerSubmissions = [
     {
-      learner_id: 125,
-      assignment_id: 1,
+      learner_id: ,
+      assignment_id: ,
       submission: {
         submitted_at: "2023-01-25",
-        score: 47
+        score: 
       }
     },
     {
-      learner_id: 125,
-      assignment_id: 2,
+      learner_id: ,
+      assignment_id: ,
       submission: {
         submitted_at: "2023-02-12",
-        score: 150
+        score: 
       }
     },
     {
-      learner_id: 125,
-      assignment_id: 3,
+      learner_id: ,
+      assignment_id: ,
       submission: {
         submitted_at: "2023-01-25",
-        score: 400
+        score: 
       }
     },
     {
-      learner_id: 132,
-      assignment_id: 1,
+      learner_id: ,
+      assignment_id: ,
       submission: {
         submitted_at: "2023-01-24",
-        score: 39
+        score: 
       }
     },
     {
-      learner_id: 132,
-      assignment_id: 2,
+      learner_id: ,
+      assignment_id: ,
       submission: {
         submitted_at: "2023-03-07",
-        score: 140
+        score: 
       }
     }
   ];
@@ -147,16 +147,16 @@ const CourseInfo = {
     // here, we would process this data to achieve the desired result.
     const result = [
       {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
+        id: ,
+        avg: , // (47 + 150) / (50 + 150)
+        1: , // 47 / 50
+        2: // 150 / 150
       },
       {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
+        id: ,
+        avg: , // (39 + 125) / (50 + 150)
+        1: , // 39 / 50
+        2:  // late: (140 - 15) / 150
       }
     ];
   
