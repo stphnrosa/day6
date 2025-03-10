8db71227f40cf07d4b83142aa2325e943ebb4cc7
stphnrosa <stphn.rosa@gmail.com>
2025-03-09 22:10:37 -0400

    fifth commit

--- sba308.js
+++ sba308.js
@@ -1,167 +1,104 @@
 // Reference Source: code sandbox, https://codesandbox.io/p/sandbox/sba-308-example-26sg4j?file=%2Fsrc%2Findex.js%3A2%2C1-100%2C1&from-embed=
-//  // 1.  course info object:
-//  {
-//   "id": number,
-//   "name": string,
-// }
+// reference source: for functions I referenced ws3 https://www.w3schools.com/js/js_functions.asp
+//  Reference Source: https://www.w3schools.com/js/js_arrays.asp
+//  Reference Source: https://www.w3schools.com/jsref/jsref_object_keys.asp
+//  Reference Source: https://www.canva.com/design/DAFxJzEGlWs/OxnBpoTDbneAidu7eN9WLw/edit
+//  Reference Source: https://www.w3schools.com/js/js_if_else.asp
+// Reference Source: https://www.youtube.com/watch?v=PgUXiprlg1k
+// Reference Source:
 
+// let generalData = {
+//   id: 333,
+//   avg: 85, // the learner’s weighted average, in which assignments with more points_possible should be counted for more e.g. a learner with 50/100 on one assignment and 190/200 on anotherwould have a weighted average score of 240/300 = 80%.
+//   learnerSubmission: 100,
+// };
 
-// 2. AssignmentGroup object
+const courseInfo = {
+  id: 777,
+  name: "Friends",
+};
 
-// {
-//     "id": number,
-//     "name": string,
-//     // the ID of the course the assignment group belongs to
-//     "course_id": number,
-//     // the percentage weight of the entire assignment group
-//     "group_weight": number,
-//     "assignments": [AssignmentInfo],
-//   }
-  
+const assignmentInfo = {
+  //assignment info block  has to be placed before assignment group because ln 30 won't work if it's after. (JS reads from top to bottom)
+  id: 444,
+  name: "How you doin'?",
+  due_at: "2025-03-15", //the due date for the assignment
+  points_possible: 500, // the maximum points possible for the assignment
+};
 
-//// 3. AssignmentInfo
-// {
-//     "id": number,
-//     "name": string,
-//     // the due date for the assignment
-//     "due_at": Date string,
-//     // the maximum points possible for the assignment
-//     "points_possible": number,
-//   }
-  
-// 4. LearnerSubmission Objects
-// {
-//     "learner_id": number,
-//     "assignment_id": number,
-//     "submission": {
-//       "submitted_at": Date string,
-//       "score": number
-//     }
-// }
+const assignmentGroup = {
+  id: 222,
+  name: "New York",
+  course_id: 100, //the ID of the course the assignment group belongs to
+  group_weight: 300, // the percentage weight of the entire assignment group
+  assignments: [assignmentInfo],
+};
 
-//***GOAL:analyze and transform this data such that the output of your program is an array of objects, each containing the following information in the following format:***
-
-
-// // EXAMPLE::::
-
-// {
-//     // the ID of the learner for which this data has been collected
-//     "id": number,
-//     // the learner’s total, weighted average, in which assignments
-//     // with more points_possible should be counted for more
-//     // e.g. a learner with 50/100 on one assignment and 190/200 on another
-//     // would have a weighted average score of 240/300 = 80%.
-//     "avg": number,
-    // each assignment should have a key with its ID,
-    // and the value associated with it should be the percentage that
-//     // the learner scored on the assignment (submission.score / points_possible)
-//     <assignment_id>: number,
-//     // if an assignment is not yet due, it should not be included in either
-//     // the average or the keyed dictionary of scores
-// }
-
-
-// ---------------------
-// Create a function named getLearnerData()
-//values(parameters)-> CourseInfo, AssignmentGroup, [LearnerSubmission]. THESE SHOULD BE AN ARAY OF OBJECTS 
-
-// SAMPLEDATA(sandbox):
-const CourseInfo = {
-    id: ,
-    name: "Introduction to JavaScript"
-  };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: ,
-    name: "Fundamentals of JavaScript",
-    course_id: ,
-    group_weight: ,
-    assignments: [
-      {
-        id: ,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 
-      },
-      {
-        id: ,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
-    {
-      learner_id: ,
-      assignment_id: ,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 
-      }
+const learnerSubmission = [
+  {
+    learner_id: 333,
+    assignment_id: 4,
+    submission: {
+      submitted_at: "2025-03-16",
+      score: 30,
     },
-    {
-      learner_id: ,
-      assignment_id: ,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 
-      }
+  },
+  {
+    learner_id: 333,
+    assignment_id: 10,
+    submission: {
+      submitted_at: "2025-04-01",
+      score: 120,
     },
-    {
-      learner_id: ,
-      assignment_id: ,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 
-      }
+  },
+  {
+    learner_id: 000,
+    assignment_id: 4,
+    submission: {
+      submitted_at: "2025-03-25",
+      score: 47,
     },
-    {
-      learner_id: ,
-      assignment_id: ,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 
-      }
+  },
+  {
+    learner_id: 000,
+    assignment_id: 10,
+    submission: {
+      submitted_at: "2025-04-12",
+      score: 179,
     },
-    {
-      learner_id: ,
-      assignment_id: ,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 
-      }
-    }
-  ];
-  
-  function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: ,
-        avg: , // (47 + 150) / (50 + 150)
-        1: , // 47 / 50
-        2: // 150 / 150
-      },
-      {
-        id: ,
-        avg: , // (39 + 125) / (50 + 150)
-        1: , // 39 / 50
-        2:  // late: (140 - 15) / 150
-      }
-    ];
-  
-    return result;
+  },
+];
+
+function getLearnerData(courseInfo, assignmentGroup, learnerSubmission) {
+  let totalScore = 0;
+  let result = {};
+  result.id = learnerSubmission[0].learner_id;
+  for (let i = 0; i < learnerSubmission.length; i++) {
+    // learn submission.length includes all the blocks of code in learnerSubmission
+    result[learnerSubmission[i].assignment_id] =
+      learnerSubmission[i].submission.score;
+    totalScore += learnerSubmission[i].submission.score;
+
+    result.avg = totalScore / learnerSubmission.length;
+    console.log(result);
   }
-  
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
+
+  let avg = result.avg;
+  if (avg >= 94) {
+    console.log("You have an A+!");
+  } else {
+    console.log("Fail");
+  }
+
+  const submittedDate =
+
+  // let completed = new date(submittedDate);
+  // const deadline = new date(assignmentInfo.due_at);
+  // if (completed > deadline) {
+  //   console.log("LATE");
+  // } else {
+  //   console.log("on time!");
+  // }
+}
+
+getLearnerData(courseInfo, assignmentGroup, learnerSubmission);
