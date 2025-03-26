a2947d06e05f9ce8d68cc5e192e627a0650c0902
stphnrosa <stphn.rosa@gmail.com>
2025-03-26 17:39:27 -0400

    third commit

--- ALAB30851.js
+++ ALAB30851.js
@@ -1,13 +1,101 @@
-// //Create reusable blocks of code with functions.
-// Implement Array object methods.
+//references: https://www.freecodecamp.org/news/how-to-add-numbers-in-javascript-arrays/
+// https://www.geeksforgeeks.org/javascript-program-to-calculate-the-average-of-all-the-elements-present-in-an-array/
+//reference: https://www.geeksforgeeks.org/how-to-get-the-longest-string-in-an-array-using-javascript/
+
+
+/////////////////////////////////////////////////
+                  //  PART 1 //
+/////////////////////////////////////////////////
+//sum of an array of numbers //
+// let myNumbers=[10,20,3,45];
+
+// let sum = 0
+
+// for(let i=0; i<myNumbers.length; i++) {
+//     sum += myNumbers[i];
+// }
+// console.log(sum)
+
+// Take an array of numbers and return the average. //
+
+// let array=[10,20,3,45];
+
+// let sum = array.reduce((acc,curr )=> acc + curr,0);
+// let average =sum/array.length; // reduce method is much simpler than other methods! :)
+
+// console.log(average)
+
+//Take an array of strings and return the longest string.//
+
+let array =[
+    "Spring",
+    "is",
+    "here!",
+];
+
+// "a" is the first string aka "Spring". "Spring" is then compared to the rest of the strings("is", "here!") to compare which word is longer( this is why we add ".length".)
+
+function longestString(){
+    return array.reduce(function(a,b){
+        return a.length > b.length ? a: b;
+    });
+} 
+console.log(longestString());
+
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+// let array =[
+//     "Eyes",
+//     "Lips",
+//     "Face",
+// ];
+// let i = 3
+// stringsLongerThan(["Eyes","Lips","Face",],2025);
+
+// Take a number, n, and print every number between 1 and n without using loops. Use recursion.//
+
+
+/////////////////////////////////////////////////
+                  //  PART 2 //
+/////////////////////////////////////////////////
+
+// Data to test work 
+ let data=
+[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+
+// Use callback functions alongside Array methods to accomplish the following:
+
+// Sort the array by age.
+
+ 
 
 
 
 
 
+//Filter the array to remove entries with an age greater than 50.
 
-// write functions that accomplish the following
-//Take an array of numbers and return the sum.
+
+
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+
+
+
+// Use the reduce method to calculate the sum of the ages.
+// Then use the result to calculate the average age.
+
+
+
+
+
+/////////////////////////////////////////////////
+                  //  PART 3 //
+/////////////////////////////////////////////////
+
+
 
 
 
